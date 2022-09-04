f816330aff530224c0bef5c42b198cda72acb1bb
Alexander Lavrov <a.lavrov@rdp.ru>
2022-09-04 17:23:39 +0300

    hw06 completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,5 +10,35 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
-	return nil
+	out := in
+	for _, i := range stages {
+		out = wrapStage(i(out), done)
+	}
+	return out
+}
+
+func wrapStage(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+			for range in {
+			}
+		}()
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
